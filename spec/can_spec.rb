37469bdcf7be2f71528160255731bd3af331a542
hiroyuki hanai <hiroyuki.hanai@gmail.com>
2018-03-24 17:57:23 +0900

    feature: add weight_cost test

--- spec/can_spec.rb
+++ spec/can_spec.rb
@@ -3,24 +3,24 @@ require "spec_helper"
 
 describe "Can#initialize" do
   subject do
-    Can.new(cost_par_can: cost_par_can, cost_par_pond: cost_par_pond, weight_par_can: weight_par_can)
+    Can.new(cost_par_can: cost_par_can, cost_par_pond: cost_par_pond, once_par_can: once_par_can)
   end
 
   context "int cost" do
     let(:cost_par_can) { 1 }
-    let(:cost_par_pond) { 1 }
-    let(:weight_par_can) { 0.4 }
+    let(:cost_par_pond) { 2 }
+    let(:once_par_can) { 0.4 }
 
     it "assigns floated cost" do
       expect(subject.instance_variable_get(:@cost_par_can)).to eq(1.0)
     end
 
     it "assigns floated pond cost" do
-      expect(subject.instance_variable_get(:@cost_par_pond)).to eq(1.0)
+      expect(subject.instance_variable_get(:@cost_par_pond)).to eq(2.0)
     end
 
     it "assigns floated weight_par_can" do
-      expect(subject.instance_variable_get(:@weight_par_can)).to eq(0.4)
+      expect(subject.instance_variable_get(:@once_par_can)).to eq(0.4)
     end
   end
 end
@@ -31,7 +31,7 @@ describe "Can#cost_by_numbers" do
   end
 
   let(:can) do
-    Can.new(cost_par_can: 1, cost_par_pond: 1, weight_par_can: 1)
+    Can.new(cost_par_can: 1, cost_par_pond: 100, once_par_can: 1)
   end
 
   context "int cost" do
@@ -41,21 +41,22 @@ describe "Can#cost_by_numbers" do
       expect(subject).to eq(1.0)
     end
   end
+end
 
-  context "float cost" do
-    let(:number_of_cans) { 2.0 }
-
-    it "assigns floated cost" do
-      expect(subject).to eq(2.0)
-    end
+describe "Can#cost_by_weight" do
+  subject do
+    can.cost_by_weight(number_of_cans)
   end
 
-  context "string cost" do
-    let(:number_of_cans) { "3.0" }
+  let(:can) do
+    Can.new(cost_par_can: 100, cost_par_pond: 1, once_par_can: 1)
+  end
+
+  context "int cost" do
+    let(:number_of_cans) { 3 }
 
     it "assigns floated cost" do
-      expect(subject).to eq(3.0)
+      expect(subject).to eq(0.1875)
     end
   end
 end
-
