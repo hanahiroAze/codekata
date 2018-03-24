f8b6bfbf078ed14966542ebc5d4430a9c63714f3
hiroyuki hanai <hiroyuki.hanai@gmail.com>
2018-03-24 17:57:48 +0900

    refactor: rename weight  -> once

--- can.rb
+++ can.rb
@@ -1,8 +1,8 @@
 class Can
-  def initialize(cost_par_can:, cost_par_pond:, weight_par_can:)
+  def initialize(cost_par_can:, cost_par_pond:, once_par_can:)
     @cost_par_can = cost_par_can.to_f
     @cost_par_pond = cost_par_pond.to_f
-    @weight_par_can = weight_par_can.to_f
+    @once_par_can = once_par_can.to_f
   end
 
   def cost_by_numbers(number_of_cans)
@@ -10,6 +10,6 @@ class Can
   end
 
   def cost_by_weight(number_of_cans)
-    @cost_par_pond * ((@weight_par_can * number_of_cans.to_f)/16)
+    @cost_par_pond * ((@once_par_can * number_of_cans.to_f)/16)
   end
 end
