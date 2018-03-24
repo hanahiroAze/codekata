4346358b9d5f5f4136950f9fbdfd0b396f81f32b
hiroyuki hanai <hiroyuki.hanai@gmail.com>
2018-03-24 17:39:49 +0900

    feature: add cost test

--- spec/can_spec.rb
+++ spec/can_spec.rb
@@ -31,3 +31,33 @@ describe "Can#initialize" do
   end
 end
 
+describe "Can#cost" do
+  subject do
+    (Can.new(1)).cost(number_of_cans)
+  end
+
+  context "int cost" do
+    let(:number_of_cans) { 1 }
+
+    it "assigns floated cost" do
+      expect(subject).to eq(1.0)
+    end
+  end
+
+  context "float cost" do
+    let(:number_of_cans) { 2.0 }
+
+    it "assigns floated cost" do
+      expect(subject).to eq(2.0)
+    end
+  end
+
+  context "string cost" do
+    let(:number_of_cans) { "3.0" }
+
+    it "assigns floated cost" do
+      expect(subject).to eq(3.0)
+    end
+  end
+end
+
