9f1584aab87ce5c70c2c7fa6c4ac077c41a91d5e
hiroyuki hanai <hiroyuki.hanai@gmail.com>
2018-03-24 17:50:09 +0900

    refactor: add initialize params / rename cost -> cost_by_numbers

--- spec/can_spec.rb
+++ spec/can_spec.rb
@@ -3,37 +3,35 @@ require "spec_helper"
 
 describe "Can#initialize" do
   subject do
-    Can.new(cost_par_can)
+    Can.new(cost_par_can: cost_par_can, cost_par_pond: cost_par_pond, weight_par_can: weight_par_can)
   end
 
   context "int cost" do
     let(:cost_par_can) { 1 }
+    let(:cost_par_pond) { 1 }
+    let(:weight_par_can) { 0.4 }
 
     it "assigns floated cost" do
       expect(subject.instance_variable_get(:@cost_par_can)).to eq(1.0)
     end
-  end
 
-  context "float cost" do
-    let(:cost_par_can) { 2.0 }
-
-    it "assigns floated cost" do
-      expect(subject.instance_variable_get(:@cost_par_can)).to eq(2.0)
+    it "assigns floated pond cost" do
+      expect(subject.instance_variable_get(:@cost_par_pond)).to eq(1.0)
     end
-  end
 
-  context "string cost" do
-    let(:cost_par_can) { "3.0" }
-
-    it "assigns floated cost" do
-      expect(subject.instance_variable_get(:@cost_par_can)).to eq(3.0)
+    it "assigns floated weight_par_can" do
+      expect(subject.instance_variable_get(:@weight_par_can)).to eq(0.4)
     end
   end
 end
 
-describe "Can#cost" do
+describe "Can#cost_by_numbers" do
   subject do
-    (Can.new(1)).cost(number_of_cans)
+    can.cost_by_numbers(number_of_cans)
+  end
+
+  let(:can) do
+    Can.new(cost_par_can: 1, cost_par_pond: 1, weight_par_can: 1)
   end
 
   context "int cost" do
